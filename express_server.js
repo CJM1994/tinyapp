e1382df41505659c90e696a56751e7f5d773110d
CJM1994 <connorjohnmullin@gmail.com>
2022-02-23 01:54:29 +0000

    form in header to submit username:value and store in cookie

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +14,11 @@ const urlDatabase = {
 };
 
 app.get('/', (req, res) => {
+  res.redirect('/urls');
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
   res.redirect('/urls');
 });
 
