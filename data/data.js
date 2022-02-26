331b9a6cab99ccff44d08efe5389e60245d17df2
CJM1994 <connorjohnmullin@gmail.com>
2022-02-26 22:43:50 +0000

    refactored data to separate file

---
+++ data/data.js
@@ -0,0 +1,18 @@
+const urlDatabase = {
+  // Example URL
+  // b6UTxQ: {
+  //   longURL: "https://www.tsn.ca",
+  //   userID: "userID"
+  // }
+};
+
+const users = {
+  // Example USER
+  // 'userID': {
+  //   id: 'userID',
+  //   email: 'user@email.com',
+  //   password: '$2a$10$IIRVCgO5NS6s3mVXd0E1Ju6j4H.PK7bNFWiEBn1/zmaOB.L2aTHlS'
+  // }
+};
+
+module.exports = {urlDatabase, users};
