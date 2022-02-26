e6a9ca641d7f8231f95170ea3951059e05e0b016
CJM1994 <connorjohnmullin@gmail.com>
2022-02-26 22:33:14 +0000

    improve documentation and readability

--- express_server.js
+++ express_server.js
@@ -35,28 +35,33 @@ const users = {
 };
 
 /* ----- GET Routes ----- */
+// Redirect to URL Table / Main Page from Root
 app.get('/', (req, res) => {
   res.redirect('/urls');
 });
 
+// Login Form
 app.get('/login', (req, res) => {
   if (req.session.user_ID) {
     res.redirect('/urls');
   } else res.render('login');
 });
 
+// Registration Form
 app.get('/register', (req, res) => {
   if (req.session.user_ID) {
     res.redirect('/urls');
   } else res.render('register');
 });
 
+// Display URL Table / Main Page
 app.get('/urls', (req, res) => {
   const urlDatabaseFiltered = filterURLs(urlDatabase, req);
   const templateVars = { urls: urlDatabaseFiltered, user: users[req.session.user_ID] };
   res.render('urls_index.ejs', templateVars);
 });
 
+// Display Add URL Form - Validate Login
 app.get('/urls/new', (req, res) => {
   if (!req.session.user_ID) {
     res.redirect('/login');
@@ -65,20 +70,24 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new', templateVars);
 });
 
+// Display Edit URL Form - Validate Login
 app.get('/urls/:shorturl', (req, res) => {
-
   if (urlDatabase[req.params.shorturl]) {
     const urlDatabaseFiltered = filterURLs(urlDatabase, req);
     const templateVars = { shortURL: req.params.shorturl, longURL: urlDatabase[req.params.shorturl].longURL, user: users[req.session.user_ID] };
+
     if (urlDatabaseFiltered[req.params.shorturl]) {
       res.render('urls_view.ejs', templateVars);
-    } else res.render('urls_view_logout', templateVars);
+    } else {
+      res.render('urls_view_logout', templateVars);
+    }
+
   } else {
     res.status(404).send('Page not found');
-  }
-
+  };
 });
 
+// Redirect User to External Site Using ShortID to Find longURL
 app.get('/u/:shorturl', (req, res) => {
   if (urlDatabase[req.params.shorturl]) {
     res.redirect(urlDatabase[req.params.shorturl].longURL);
@@ -88,8 +97,8 @@ app.get('/u/:shorturl', (req, res) => {
 });
 
 /* ----- POST Routes ----- */
+// Login - Authenticate & Set Cookies
 app.post('/login', (req, res) => {
-
   const userID = lookupIDByEmail(req.body.email, users);
 
   if (!userID) {
@@ -109,6 +118,7 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+// Register Validate Input Data & Set Cookies
 app.post('/register', (req, res) => {
 
   if (req.body.email === '' || req.body.password === '') {
@@ -134,6 +144,7 @@ app.post('/register', (req, res) => {
   }
 });
 
+// Generate New URL ID & Add to urlDatabase - Validate Login
 app.post('/urls', (req, res) => {
   if (req.session.user_ID) {
     const urlID = generateRandomString();
